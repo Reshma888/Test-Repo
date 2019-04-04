e3cf937bcd75f3c4fe81e67922b4d45dce128fb4
Reshma888 <47694560+Reshma888@users.noreply.github.com>
2019-04-04 21:07:36 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,38 +1,15 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data:" ",
-    }
-    this.displayMessage = this.displayMessage.bind(this)
-  };
-  displayMessage(){
-    const date = new Date();
-    const hour = date.getHours();
-    const username = "Reshma"
-    if (hour < 12){
-      this.setState({data: ` Good Morning ${username} `})
-    }else if  (hour < 18){
-      this.setState({data: `Good Afternoon ${username}`})
-    }else {
-      this.setState({data: `Goodevening ${username}`})
-    }
-
-  }
-  render () {
+  render() {
     return (
       <div className="App">
-      <Button onClick = {this.displayMessage}>click</Button>
-      <h1>{this.state.data}</h1>
-      </div>
+        <h2>Hello World!</h2>
+          </div>
     );
   }
-
-  }
+}
 
 export default App;
